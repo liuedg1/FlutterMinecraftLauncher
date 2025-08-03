5ed9c50ad74857266f8b34dc79a392695fd3c73c
liuedg <liuedg@hotmail.com>
2025-08-03 12:31:37 +0800

    feat(ui): add Noto Sans SC

--- lib/main.dart
+++ lib/main.dart
@@ -1,65 +1,60 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(const FMCLBaseApp());
 
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class FMCLBaseApp extends StatelessWidget {
+  const FMCLBaseApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Minecraft Launcher',
+
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+
+        fontFamily: 'Noto Sans SC', // Use NotoSans
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+
+      home: const HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
-  final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: Text('Flutter Minecraft Launcher'),
       ),
+
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('You have pushed the button this many times:'),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+                '测试文本;Test Text',
+              style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.w600
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+
     );
   }
 }
