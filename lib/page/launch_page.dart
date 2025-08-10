c871d54bf719e8b3fa85de0ca61770fef5432aa1
liuedg <liuedg@hotmail.com>
2025-08-10 12:11:53 +0800

    feat: add FAB in launch page

--- lib/page/launch_page.dart
+++ lib/page/launch_page.dart
@@ -1,15 +1,36 @@
 import 'package:flutter/material.dart';
 
-class LaunchPage extends StatelessWidget {
+import '../constants.dart';
+
+//TODO:Custom page by user (e.g. display news)
+class LaunchPage extends StatefulWidget {
   const LaunchPage({super.key});
 
   @override
+  State<StatefulWidget> createState() => _LaunchPageState();
+}
+
+class _LaunchPageState extends State<LaunchPage> {
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        'Launch Page',
-        style: TextStyle(fontSize: 24),
+    return Scaffold(
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: _launchCurrentMinecraft,
+        label: const Text('Launch'),
+        icon: const Icon(Icons.rocket_launch),
+      ),
+
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          const SizedBox(width: kDefaultPadding / 2),
+          const Expanded(
+            child: Text(kAppNameAbb, style: TextStyle(fontSize: 28)),
+          ),
+        ],
       ),
     );
   }
+
+  void _launchCurrentMinecraft() {}
 }
